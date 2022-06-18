2e11b71d40a4f6cfa20a46f616ef8ea485e88026
giacomocerquone <cerquone96@hotmail.it>
2022-06-18 12:14:46 +0200

    fix: baseurl, add sh to simplify deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "https://giacomocerquone.github.io/react-native-use-sheet/",
-  baseUrl: "/",
+  url: "https://giacomocerquone.github.io/",
+  baseUrl: "/react-native-use-sheet/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
